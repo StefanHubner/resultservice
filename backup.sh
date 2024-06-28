42fe7cd90945b4968433606615a3dbdbca8829a5
Stefan Hubner <stefan.hubner@economics.ox.ac.uk>
2024-06-28 18:52:56 +0100

    quotes closed

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo -u postgres pg_dump ds > /app/static/sql_backup/daily.backup.sql
-aws s3 --region eu-west-1 --profile s3hubnerinfo cp /app/static/ s3://resultservice-backup/ --recursive --include "*" --exclude "smHmip*
+aws s3 --region eu-west-1 --profile s3hubnerinfo cp /app/static/ s3://resultservice-backup/ --recursive --include "*" --exclude "smHmip*"
